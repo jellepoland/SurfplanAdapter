f9bc6f0bf20de9a9c947a2ed231202a3905c2c4d
corentintran <corentin.tran@gmail.com>
2024-08-06 17:05:29 +0200

    add change of reference frame and update plotting and main

---
+++ src/SurfplanAdapter/transforming_coordinate_system.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def rotate_surfplan_to_VSM(coord_surfplan):
+    """
+    Transform coordinate from Surfplan reference frame to VSM reference frame
+
+    Surfplan reference frame :
+    # z: along the chord / parallel to flow direction
+    # x: left
+    # y: upwards
+
+    VSM reference frame :
+    # Body EastNorthUp (ENU) Reference Frame (aligned with Earth direction)
+    # x: along the chord / parallel to flow direction
+    # y: left
+    # z: upwards
+
+    Parameters:
+    coord_surfplan (tuple): a tuple of three floats representing the x, y, and z coordinates of the rib endpoint in Surfplan reference frame.
+ 
+    Returns:
+    coord_vsm (tuple): a tuple of three floats representing the x, y, and z coordinates of the rib endpoint in VSM reference frame.
+    """
+
+    # Rotation matrix
+    R = np.array([
+        [0, 0, 1],
+        [1, 0, 0],
+        [0, 1, 0]
+    ])
+
+    coord_vsm = np.dot(R, coord_surfplan)
+
+    return coord_vsm
