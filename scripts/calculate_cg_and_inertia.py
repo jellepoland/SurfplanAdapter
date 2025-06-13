1170e154393f90a68e46b3608b368fc4c781b9af
jellepoland <jellepoland@gmail.com>
2025-06-13 12:25:04 +0200

    Updating cg and intertia calculation to work with yaml input

--- scripts/calculate_cg_and_inertia.py
+++ scripts/calculate_cg_and_inertia.py
@@ -1,48 +1,43 @@
 from pathlib import Path
-from SurfplanAdapter import calculate_cg_and_inertia
+from SurfplanAdapter import calculate_cg_and_inertia, generate_wing_yaml
 from SurfplanAdapter.utils import PROJECT_DIR
-from SurfplanAdapter import generate_geometry_csv_files
 
-if __name__ == "__main__":
 
-    ### User inputs
-    data_folder_name = "TUDELFT_V3_KITE"
-    kite_file_name = "TUDELFT_V3_KITE_3d"
-    kite_name = "TUDELFT_V3_KITE"
+def main(
+    kite_name="TUDELFT_V3_KITE",
+    total_wing_mass=10.0,
+    canopy_kg_p_sqm=0.05,
+    le_to_strut_mass_ratio=0.7,
+    sensor_mass=0.5,
+    is_show_plot=True,
+    desired_point=[0, 0, 0],
+):
+    """Calculate CG and inertia for a given kite_name. Requires config_kite.yaml to exist or will generate it."""
+    data_dir = Path(PROJECT_DIR) / "data" / f"{kite_name}"
+    path_surfplan_file = Path(data_dir) / f"{kite_name}.txt"
+    save_dir = Path(PROJECT_DIR) / "processed_data" / f"{kite_name}"
+    yaml_file_path = Path(save_dir) / "config_kite.yaml"
 
-    ## Creating Paths
-    path_surfplan_file = (
-        Path(PROJECT_DIR) / "data" / f"{data_folder_name}" / f"{kite_file_name}.txt"
-    )
-    dir_to_save_in = Path(PROJECT_DIR) / "processed_data" / f"{data_folder_name}"
-
-    ## Create geometry file once
-    geometry_file_path = Path(dir_to_save_in) / "wing_geometry.csv"
-    if not geometry_file_path.exists():
-        data_dir = Path(PROJECT_DIR) / "data" / f"{data_folder_name}"
-        save_dir = Path(PROJECT_DIR) / "processed_data" / f"{data_folder_name}"
-
-        generate_geometry_csv_files.main(
+    # Generate YAML config if it does not exist
+    if not yaml_file_path.exists():
+        generate_wing_yaml.main(
             path_surfplan_file=path_surfplan_file,
             save_dir=save_dir,
             profile_load_dir=Path(data_dir) / "profiles",
             profile_save_dir=Path(save_dir) / "profiles",
+            airfoil_type="masure_regression",
         )
 
-    file_path = Path(PROJECT_DIR) / "processed_data" / f"{kite_name}" / "geometry.csv"
-    total_wing_mass = 10.0
-    canopy_kg_p_sqm = 0.05
-    le_to_strut_mass_ratio = 0.7
-    sensor_mass = 0.5
-    is_show_plot = True
-    desired_point = [0, 0, 0]
-
     calculate_cg_and_inertia.main(
-        file_path,
-        total_wing_mass,
-        canopy_kg_p_sqm,
-        le_to_strut_mass_ratio,
-        sensor_mass,
+        yaml_file_path,
+        total_wing_mass=total_wing_mass,
+        canopy_kg_p_sqm=canopy_kg_p_sqm,
+        le_to_strut_mass_ratio=le_to_strut_mass_ratio,
+        sensor_mass=sensor_mass,
         desired_point=desired_point,
         is_show_plot=is_show_plot,
     )
+
+
+if __name__ == "__main__":
+    main()
